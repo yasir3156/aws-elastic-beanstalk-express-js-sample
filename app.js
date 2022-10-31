9b1b97fcce884170c75867a519f9b86a91549dfc
yasir3156 <112001065+yasir3156@users.noreply.github.com>
2022-10-31 22:28:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Parh la bhai'));
+app.get('/', (req, res) => res.send('Hello world'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
